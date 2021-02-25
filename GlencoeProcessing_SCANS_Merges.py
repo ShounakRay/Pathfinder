a814f292019aa014990229bf4d4ec9031d4b60dc
Shounak Ray <rijshouray@gmail.com>
2021-02-24 23:09:55 -0700

    Member Feature Engineering

--- GlencoeProcessing_SCANS_Merges.py
+++ GlencoeProcessing_SCANS_Merges.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: scan_analysis.py
 # @Last modified by:   Ray
-# @Last modified time: 24-Feb-2021 16:02:91:914  GMT-0700
+# @Last modified time: 24-Feb-2021 22:02:27:277  GMT-0700
 # @License: [Private IP]
 
 import re
@@ -17,16 +17,25 @@ import pandas as pd
 
 def dep_type(row):
     if(row['Dependent']):
-        print(row[0].split('-'))
-        if(int(row[0].split('–')[1]) == 1):
+        seg = int(row['lettered_ID'].split('-')[1])
+        print(seg)
+        if(seg == 1):
             return 'Spousal'
-        elif(int(row[0].split('–')[1]) > 1):
+        elif(seg > 1):
             return 'Non-Spousal'
         else:
             return 'Unclassified'
     else:
         return None
 
+
+group_relations = {'GC': 'Golf Club',
+                   'AFF': 'Affiliate',
+                   'INT': 'Internal (INT)',
+                   'GL': 'Internal (GL)',
+                   'DCGuest': 'Downtown Guest',
+                   'LG': 'League',
+                   'CORP': 'Corporate'}
 
 df_MANSCANS = pd.read_csv("Data/MANUAL_SCANS.csv", low_memory=False)
 df_SCANS = pd.read_csv("Data/SCANS.csv", low_memory=False)
@@ -41,13 +50,15 @@ for d in dfs:
     store.append(set(d['member_number']))
 unique_mids = list(set(list(chain.from_iterable(store))))
 lettered_mids = [mid for mid in unique_mids if(type(mid) == str and any(let.isalpha() for let in mid))]
-lmids = pd.DataFrame(lettered_mids)
-lmids['Type'] = lmids[0].apply(lambda x: re.sub(r'[0-9]', '', x).replace('-', '').strip())
-lmids['Dependent'] = lmids[0].apply(lambda x: True if('-' in x) else False)
+lmids = pd.DataFrame(lettered_mids, columns=['lettered_ID'])
+lmids['Mem_Category_Abbrev'] = lmids['lettered_ID'].apply(lambda x: re.sub(r'[0-9]', '', x).replace('-', '').strip())
+lmids['Mem_Category_Type'] = [chg for chg in lmids['Mem_Category_Abbrev'].replace(group_relations)
+                              if(chg in group_relations.keys()) else None]
+lmids['Dependent'] = lmids['lettered_ID'].apply(lambda x: True if('-' in x) else False)
 lmids['Dependent_Type'] = lmids.apply(dep_type, axis=1)
 
-
-Counter(lmids['Type'])
+Counter(lmids['Mem_Category_Type'])
+Counter(lmids['Mem_Category_Abbrev'])
 
 mid = unique_mids[0]
 
