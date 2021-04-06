8f009b30a624fa362fbe1a08ae5480b953259fb0
Shounak Ray <rijshouray@gmail.com>
2021-04-06 13:33:50 -0600

    Check Time Analysis

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -3,7 +3,7 @@
 # @Email:  rijshouray@gmail.com
 # @Filename: xml_to_csv.py
 # @Last modified by:   Ray
-# @Last modified time: 03-Apr-2021 22:04:06:069  GMT-0600
+# @Last modified time: 06-Apr-2021 13:04:09:091  GMT-0600
 # @License: [Private IP]
 
 # CONVERSION RUN-TIME: 3.5 SECONDS (EXCLUDING OPTIONAL DATETIME TYPE CHECKS)
@@ -12,6 +12,7 @@
 import re
 from collections import Counter
 
+import adjustText
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -27,37 +28,62 @@ the_xml = the_xml[the_xml.index('<ss:Table>') + 1:the_xml.index('</ss:Table>') -
 # Retrive rows and headers (short-circuited, very optimized)
 rows, headers = [], []
 for line in the_xml:
-    '"s27"' in line and headers.append(re.findall(r'>(.*?)<', line)[1])
+    '"s27"' in line and headers.append(re.findall(r'>(.*?)<', line)[1].replace(' ', '_'))
     ('<ss:Cell>' in line or '"s21"' in line) and rows.append(re.findall(r'>(.*?)<', line)[1])
 rows = [rows[len(headers) * start:len(headers) * (start + 1)] for start in range(int(len(rows) / len(headers)))]
 
 # Transform Parsed Data
-df = pd.DataFrame(rows, columns=headers).apply(pd.to_numeric, errors='ignore')
+df = pd.DataFrame(rows, columns=headers).apply(pd.to_numeric, errors='ignore').infer_objects()
 
 # This part is optional, I personally like converting dates from string to datetime format
-df['Check Creation Date'] = pd.to_datetime(df['Check Creation Date'])
-for c in ['Check Open Time', 'Check Close Time']:
+df['Check_Creation_Date'] = pd.to_datetime(df['Check_Creation_Date'])
+for c in ['Check_Open_Time', 'Check_Close_Time']:
     df[c] = [t.time() for t in pd.to_datetime(df[c])]
+
+df = df.replace('', None)
+
+# Format Object-Typed Features
+rep_check = {}
+for col in [c for c in df.select_dtypes(object).columns
+            if not any([True if d in c else False for d in ['Time', 'Date']])]:
+    df[col] = [re.sub(r'\s{2,}', ' ', e.replace('&amp;', '&').replace(',', ', ')).strip() for e in df[col]]
+    rep_check[col] = dict(Counter(df[col]).most_common())
 
 df.to_csv('Data/Check_HighRes.csv')
 
-df['Check_Open_Duration'] = (df['Check Close Time'].apply(t_s) - df['Check Open Time'].apply(t_s)) / 60.0
+# Feature Engineering
+df['Check_Open_Duration'] = (df['Check_Close_Time'].apply(t_s) - df['Check_Open_Time'].apply(t_s)) / 60.0
 
-# Counter(df['Check_Open_Duration'] >= 0)
-# Counter(df[df['Check_Open_Duration'] < 0]['Check Close Time'])
-# plt.plot(df[df['Check_Open_Duration'] < 0]['Check Close Time'].apply(t_s).reset_index(drop=True))
-#
-#
-# df['Check_Open_Duration']
-# plt.figure(figsize=(30, 15))
-# plt.plot(df[df['Check_Open_Duration'] >= 0]['Check_Open_Duration'])
-# plt.plot(df[df['Check_Open_Duration'] < 0]['Check_Open_Duration'])
-# plt.plot(df[(df['Check_Open_Duration'] < 0) & (df['Check Open Time'].apply(t_s) < 0)]['Check_Open_Duration'])
-# Counter(df['Check Open Time'].apply(t_s) != 0)
-# dir(df['Check Close Time'][0])
-#
-# ((df['Check Close Time'][0].hour * 60) + df['Check Close Time'][0].minute) * 60
+# _temp = df[(df['Check_Open_Duration'] < 0) & (df['Check_Close_Time'].apply(t_s) != 0)]
 
+# Visualizations (Check Times)
+if __name__ == '__main__':
+    fig, ax = plt.subplots(figsize=(15, 15))
+    _temp = df[df['Check_Open_Duration'] >= 0]['Check_Open_Duration']
+    _ = ax.scatter(_temp.index, _temp,
+                   s=3, c='green', label='Close_Time after Open_Time (Makes Sense)')
+    _temp = df[df['Check_Open_Duration'] < 0]['Check_Open_Duration']
+    _ = ax.scatter(_temp.index, _temp,
+                   s=10, c='orange', label='Close_Time before Open_Time (No Sense)')
+    _temp = df[(df['Check_Open_Duration'] < 0) & (df['Check_Close_Time'].apply(t_s) != 0)]
+    _ = ax.scatter(_temp['Check_Open_Duration'].index, _temp['Check_Open_Duration'],
+                   s=50, c='red', label='Close_Times Don\'t make sense')
+    _ = ax.legend()
+    _ = ax.set_xlabel('Table Row Number (proportional to Time)')
+    _ = ax.set_ylabel('Check_Open_Duration')
+    ax.grid(which='major')
+    ax.grid(which='minor')
+    ax.minorticks_on()
+    ax.tick_params(labeltop=True, labelright=True)
+    texts = []
+    for i in _temp.index:
+        y_pos = _temp['Check_Open_Duration'][i]
+        texts.append(ax.text(i, y_pos, 'Row ' + str(i) + ' @ ' + str(int(round(y_pos))) + ' mins', size=7))
+        # ax.annotate('Row ' + str(i), (i + 2000, _temp['Check_Open_Duration'][i] + 20), fontsize=6)
+    _ = adjustText.adjust_text(texts)
+    fig.tight_layout()
+
+# Visualizations (Other)
 if __name__ == '__main__':
     from collections import Counter
 
@@ -65,12 +91,12 @@ if __name__ == '__main__':
     import seaborn as sns
 
     # Check Activity Over Time
-    _time, _freq = (zip(*sorted(dict(Counter(df['Check Creation Date'])).items())))
+    _time, _freq = (zip(*sorted(dict(Counter(df['Check_Creation_Date'])).items())))
     time_freq = pd.Series(_freq, _time)
     fig, ax = plt.subplots(figsize=(15, 20), nrows=3)
     ax[0].set_title('Check Activity on Day of Week\n(Sunday > Saturday)')
     sns.boxplot((time_freq.index.dayofweek + 1) % 7 + 1, time_freq, ax=ax[0])
     ax[1].set_title('Check Activity on Week of Year\nNOTE: Week 48 – 52 is from 2019, Week 1 – 5 is from 20')
-    sns.boxplot(time_freq.index.weekofyear, time_freq, ax=ax[1])
+    sns.boxplot(time_freq.index.weekofyear, time_freq, ax=ax[1], order=time_freq.index.weekofyear.unique())
     ax[2].set_title('Check Activity in Given Data')
     ax[2].bar(_time, _freq)
